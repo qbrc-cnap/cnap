753a8e242b4557005e8445314b419c664a90d3e5
brian <blawney@hsph.harvard.edu>
2018-11-26 12:25:42 -0500

    Adding test module.  No actual tests yet.

--- analysis/tests.py
+++ analysis/tests.py
@@ -1,3 +1,13 @@
-from django.test import TestCase
+import unittest
 
-# Create your tests here.
+class ViewUtilsTest(unittest.TestCase):
+    '''
+    This test class covers the "background" parts 
+    of the view.  It is assumed that the Workflow has
+    already been imported and is live (e.g. users can access)
+    the workflow url
+    '''
+    def test_missing_gui_spec_raises_exception(self):
+        pass
+
+
