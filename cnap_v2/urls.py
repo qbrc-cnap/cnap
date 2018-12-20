f0baf13a050b34614695fd6c6b6c15c40be00c23
root <blawney@hsph.harvard.edu>
2018-12-20 16:44:49 +0000

    Moving around some static resources.  Moved out of transfer_app and into base

--- cnap_v2/urls.py
+++ cnap_v2/urls.py
@@ -3,7 +3,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.urls import path, re_path, include
 
-import views as base_views
+from . import views as base_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
