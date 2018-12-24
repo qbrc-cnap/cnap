d0bdf4a425c743fee7b128b97677d0a1c4faff0a
root <blawney@hsph.harvard.edu>
2018-12-24 20:47:07 +0000

    Adding more fields to admin for Workflow

--- analysis/admin.py
+++ analysis/admin.py
@@ -3,8 +3,8 @@ from django.contrib import admin
 from .models import Workflow
 
 class WorkflowAdmin(admin.ModelAdmin):
-    list_display = ('workflow_name', 'workflow_id', 'version_id', 'is_default', 'is_active')
-    list_editable = ('is_default', 'is_active')
+    list_display = ('workflow_name', 'workflow_id', 'version_id', 'is_default', 'is_active', 'workflow_title', 'workflow_description')
+    list_editable = ('is_default', 'is_active', 'workflow_title', 'workflow_description')
     list_display_links = ('workflow_name',)
 
 admin.site.register(Workflow, WorkflowAdmin)
