c5dd730b9753241930bd5638f34cc7c3808676e9
root <blawney@hsph.harvard.edu>
2019-02-06 17:31:25 +0000

    Fixing small bug which required cast

--- transfer_app/views.py
+++ transfer_app/views.py
@@ -222,7 +222,7 @@ class TransferComplete(APIView):
 
                             # did they use the last download?  If so, set the Resource inactive
                             if (resource.total_downloads + 1) >= \
-                                settings.CONFIG_PARAMS['maximum_downloads']:
+                                int(settings.CONFIG_PARAMS['maximum_downloads']):
                                 resource.is_active = False
                             else: # still more downloads allowed.  increment
                                 resource.total_downloads += 1
