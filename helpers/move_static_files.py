74a9a63d6452ed810629ac33e2c34ea3606a162a
blawney <blawney@hsph.harvard.edu>
2020-03-11 17:10:52 -0400

    Fixing static mover file

--- helpers/move_static_files.py
+++ helpers/move_static_files.py
@@ -1,8 +1,11 @@
 import os
+import sys
 import shutil
 import django
 from django.conf import settings
 
+os.chdir(os.path.dirname(os.path.realpath(__file__)))
+sys.path.append(os.path.realpath(os.pardir))
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'cnap_v2.settings')
 django.setup()
 
@@ -12,6 +15,6 @@ src_dir = settings.STATIC_ROOT
 dir_contents = [os.path.join(src_dir, x) for x in os.listdir(src_dir)]
 for x in dir_contents:
     if os.path.isdir(x):
-        shutil.copytree(x, '/%s/%s' % (dest_dir, os.path.basename(x)))
+        shutil.copytree(x, '/%s/static/%s' % (dest_dir, os.path.basename(x)))
     else:
-        shutil.copy(x, '/%s/' % dest_dir)
+        shutil.copy(x, '/%s/static/' % dest_dir)
